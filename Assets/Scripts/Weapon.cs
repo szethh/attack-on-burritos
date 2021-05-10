54c229ee56fa59cd63f9c1e366ed10eca72640d8
MateoPeri <mateo@meligrana.com>
2021-05-11 01:44:41 +0200

    sprites and missing scripts from previous commits

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Weapon : MonoBehaviour
+{
+    public WeaponStats stats;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _renderer;
+    private BoxCollider2D _col;
+    private float _initialTime;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _col = GetComponent<BoxCollider2D>();
+
+        _rb.AddTorque(0.5f, ForceMode2D.Force);
+        _rb.AddForce(Random.insideUnitCircle*10f, ForceMode2D.Force);
+        _renderer.sprite = stats.sprite;
+        // adjust collider to sprite
+        _col.offset = _renderer.bounds.center - transform.localPosition;
+        // adjust for pivot on the left side
+        _col.offset += new Vector2(_renderer.bounds.extents.x, 0);
+        _col.size = _renderer.bounds.size*2;
+        _initialTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time > _initialTime + 7)
+        {
+            Die();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent<Player>(out var p))
+        {
+            p.PickUp(this);
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        // particles, sound, etc
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponStats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Weapon", order = 0)]
+public class WeaponStats : ScriptableObject
+{
+    public new string name;
+    public Sprite sprite;
+    public float fireRate = 3f;
+    public int weaponDamage = 1;
+    public Vector2 spread;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 5f;
+    public int bulletsPerShot = 1;
+    public Color bulletTint = Color.white;
+    public AudioClip[] shootingClips;
+    public bool piercing;
+    public float range = 20f;
+    public Vector3 firingPoint;
+    public int ammo = 10;
+    public Color laser = Color.clear;
+}
